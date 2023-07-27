bd3ed8be1cd751c56e168a296531d0f88170c42d
Hector Magana <hectormagana8423@gmail.com>
2023-07-27 11:46:00 -0600

    added router links to navbar nav.links

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -3,20 +3,35 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 // import NavDropdown from 'react-bootstrap/NavDropdown';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Dashboard from '../Pages/Dashboard';
+import { Link, Routes, Route } from 'react-router-dom';
+import Leagues from '../Pages/Leagues';
+import Games from '../Pages/Games';
+import Login from './Login';
 
 export default function newNavbar() {
 
     return(
-        <Navbar bg="dark" data-bs-theme="dark">
-            <Container>
-                <Navbar.Brand href="#home">Dashboard</Navbar.Brand>
-                <Nav className="me-auto">
-                    <Nav.Link href="#home">Leagues</Nav.Link>
-                    <Nav.Link href="#features">Games</Nav.Link>
-                    <Nav.Link href="#pricing">Log In</Nav.Link>
-                </Nav>
-            </Container>
-        </Navbar>
+        <>
+            <Navbar sticky='top' bg="dark" data-bs-theme="dark">
+                <Container>
+                    <Navbar.Brand as={Link} to='/'>Dashboard</Navbar.Brand>                
+                    <Nav className="me-auto">
+                            <Nav.Link as={Link} to='/leagues'>Leagues</Nav.Link>
+                            <Nav.Link as={Link} to='/games'>Games</Nav.Link>
+                            <Nav.Link as={Link} to='/log-in'>Log In</Nav.Link>
+                    </Nav>                
+                </Container>
+            </Navbar>
+            <div>
+                <Routes>
+                    <Route path='/' element={<Dashboard />} />
+                    <Route path='/leagues' element={<Leagues />} />
+                    <Route path='/games' element={<Games />} />
+                    <Route path='/log-in' element={<Login />} />
+                </Routes>
+            </div>
+        </>
     )
 }
     
